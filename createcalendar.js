d5b1690b2a4c2928bb7252495dcf65a6d49d9b36
pgodlevska <polina.godlevska@gmail.com>
2013-11-29 16:29:18 +0200

    Date picking works, calendar show/hide works

--- createcalendar.js
+++ createcalendar.js
@@ -1,33 +1,33 @@
-function monthInWeeks(currDate, startWeek) {
-    // Day week starts: 0 - Sunday, 1 - Monday and so on.
+function monthInWeeks(dateSource, startWeek) {
+    // Day week starts: 0 - Sunday, 1 - Monday and so on till 6 - Saturday.
     // 0 - Sunday by default.
     startWeek = startWeek || 0;
-    startWeek %= 7;
 
     var daysInWeeks = new Array();
-    // First day of month defined by given date currDate
-    var d = new Date(currDate.getFullYear(), currDate.getMonth(), 1);
-    var firstOfNext = new Date(currDate.getFullYear(),
-                               currDate.getMonth() + 1, 1);
+    // First day of month defined by given date dateSource
+    var d = new Date(dateSource.getFullYear(), dateSource.getMonth(), 1);
+    var firstOfNextMonth = new Date(dateSource.getFullYear(),
+                                    dateSource.getMonth() + 1, 1);
     // If 1st day of month is not the start of the week
+    // Look for latest start of the week in previous month
     if (d.getDay() != startWeek) {
-        // Look for latest start of the week in previous month
         d.setDate(d.getDate() - d.getDay() + startWeek);
         }
+
     var i = 0;
     // All weeks with this month days
-    alert
-    while (d < firstOfNext) {
-        daysInWeeks[i] = new Array();
+    while (d < firstOfNextMonth) {
         // Arrange days in weeks
+        daysInWeeks[i] = new Array();
         for (var j = 0; j < 7; j++) {
             daysInWeeks[i][j] = d.getDate();
-            d.setDate(d.getDate() + 1)
+            d.setDate(d.getDate() + 1);
         }
         i++;
    }
    return daysInWeeks;
 }
+
 
 function dateString(dateSource) {
     var dateString;
@@ -39,6 +39,7 @@ function dateString(dateSource) {
     return dateString;
 }
 
+
 function dateFromString(dateString) {
     date = new Date();
     date.setFullYear(parseInt(dateString.substring(0,4)));
@@ -47,7 +48,8 @@ function dateFromString(dateString) {
     return date;
 }
 
-function setNode(nodeType, nodeParent, nodeClass, nodeContent) {
+
+function setNode(nodeType, nodeParent, nodeClass, nodeContent, nodeId) {
     var newNode = document.createElement(nodeType);
     nodeParent.appendChild(newNode);
     if (nodeClass) {
@@ -56,32 +58,21 @@ function setNode(nodeType, nodeParent, nodeClass, nodeContent) {
     if (nodeContent) {
         newNode.innerHTML = nodeContent;
     }
+    if (nodeId) {
+        newNode.id = nodeId;
+    }
     return newNode;
 }
 
 
-function fillWeek(weekNode, daysArray, letters, weekNo) {
+function fillWeek(weekNode, daysArray, letters) {
     letters = letters || false;
+
     var day;
-    var today = new Date();
     for (var i = 0; i < 7; i++) {
         day = setNode("span", weekNode, CSS_REF.day, daysArray[i]);
         if (letters) {
             day.className += CSS_REF.sequelDayOfWeek;
-        } else {
-            // Find days from other months
-            if (weekNo < 1 && daysArray[i] > 7 ||
-                weekNo > 1 && daysArray[i] < 7) {
-                day.className += CSS_REF.sequelDayNotMonth;
-            } else {
-                day.className += CSS_REF.sequelDayRegular;
-                //TODO: Today only in current month
-                // Maybe somewhere out, because regular days
-                // should be accessed later anyway
-                if (daysArray[i] == today.getDate()) {
-                    day.className += CSS_REF.sequelDayToday;
-                }
-            }
         }
         if (i == 0) {
             day.className += CSS_REF.sequelDayFirst;
@@ -91,15 +82,36 @@ function fillWeek(weekNode, daysArray, letters, weekNo) {
     }
 }
 
+
 function renderMonth(dateSource) {
+    var today = new Date();
     var days = monthInWeeks(dateSource);
     var monthDays = document.createElement("div");
+
     for (var i = 0; i < days.length; i++) {
         week = setNode("div", monthDays);
-        fillWeek(week, days[i], false, i);
+        fillWeek(week, days[i]);
+        daySpans = week.getElementsByTagName("span");
+        for (var j = 0; j < daySpans.length; j++) {
+            dayNo = parseInt(daySpans[j].innerHTML);
+            // Find days not from rendered month
+            if (i < 1 && dayNo > 7 ||
+                i > 1 && dayNo < 7) {
+                daySpans[j].className += CSS_REF.sequelDayNotMonth;
+            } else {
+                daySpans[j].className += CSS_REF.sequelDayRegular;
+                // Find and highlight today
+                if (dateSource.getFullYear() == today.getFullYear() &&
+                    dateSource.getMonth() == today.getMonth() &&
+                    dayNo == today.getDate()) {
+                        daySpans[j].className += CSS_REF.sequelDayToday;
+                }
+            }
+        }
     }
     return monthDays;
 }
+
 
 function switchMonth(dateSource) {
     var monthInst = document.getElementById(DOM_ID.monthInst);
@@ -110,6 +122,7 @@ function switchMonth(dateSource) {
     var monthDays = renderMonth(dateSource);
     monthBody.replaceChild(monthDays, monthBody.lastChild);
 }
+
 
 /* CSS reference */
 var CSS_REF = {
@@ -127,12 +140,14 @@ var CSS_REF = {
     sequelDayToday: " today"
 };
 
+/* Needed ID's */
 DOM_ID ={
     arrowBack: "arrow_back",
     arrowForward: "arrow_forward",
     monthInst: "month_name",
     yearInst: "year_name",
-    monthBody: "month_body"};
+    monthBody: "month_body"
+};
 
 /* Content Constants */
 var CONT = {
@@ -145,35 +160,38 @@ var CONT = {
     dayShortNames: ["S", "M", "T", "W", "T", "F", "S"]
 };
 
+
 function createCalendar(container, dateSource){
     // Calendar layout
     container.className = CSS_REF.container;
     var field = setNode("div", container, CSS_REF.field);
+
+    // Month Header
     var monthHeader = setNode("div", field, CSS_REF.monthHeader);
     var arrowBack = setNode("span", monthHeader, CSS_REF.arrowBack,
-                            CONT.arrowBack);
+                            CONT.arrowBack, DOM_ID.arrowBack);
     var monthInst = setNode("span", monthHeader, '',
-                            CONT.monthNames[dateSource.getMonth()]);
+                            CONT.monthNames[dateSource.getMonth()],
+                            DOM_ID.monthInst);
     var yearInst = setNode("span", monthHeader, '',
-                           dateSource.getFullYear());
+                           dateSource.getFullYear(), DOM_ID.yearInst);
     var arrowForward = setNode("span", monthHeader, CSS_REF.arrowForward,
-                               CONT.arrowForward);
-    var monthBody = setNode("div", field);
+                               CONT.arrowForward, DOM_ID.arrowForward);
 
+    // Month
+    var monthBody = setNode("div", field, '', '', DOM_ID.monthBody);
+    // Header with week days
     var weekHead = setNode("div", monthBody);
     fillWeek(weekHead, CONT.dayShortNames, true);
+    // Month days
     var monthDays = renderMonth(dateSource);
     monthBody.appendChild(monthDays);
-    arrowBack.id = DOM_ID.arrowBack;
-    arrowForward.id = DOM_ID.arrowForward;
-    monthInst.id = DOM_ID.monthInst;
-    yearInst.id = DOM_ID.yearInst;
-    monthBody.id = DOM_ID.monthBody;
 }
 
+
 window.onload = function() {
-    var currentDate;
     var dateInput = document.getElementById("date_input");
+    var currentDate;
     if (dateInput.value) {
         currentDate = dateFromString(dateInput.value);
     } else {
@@ -184,17 +202,20 @@ window.onload = function() {
     dateInput.onfocus = function() {
         if (!container.hasChildNodes()) {
             createCalendar(container, currentDate);
-            var monthBody = document.getElementById(DOM_ID.monthBody);
+            // Month back handler
             var monthBack = document.getElementById(DOM_ID.arrowBack);
             monthBack.onclick = function() {
                 currentDate.setMonth(currentDate.getMonth() - 1);
                 switchMonth(currentDate);
             };
+            // Month forward handler
             var monthBack = document.getElementById(DOM_ID.arrowForward);
             monthBack.onclick = function() {
                 currentDate.setMonth(currentDate.getMonth() + 1);
                 switchMonth(currentDate);
             };
+            // Pick a date handler
+            var monthBody = document.getElementById(DOM_ID.monthBody);
             monthBody.onclick = function(e) {
                 var day = e.target;
                 var dayClass = day.className;
@@ -205,10 +226,23 @@ window.onload = function() {
                     container.style.display = "none";
                 }
             };
+            // Hide calendar
+            document.onclick = function(e) {
+                target = e.target;
+                var show = false;
+                while (target != this) {
+                    if (target == container || target == dateInput) {
+                        show = true;
+                        break;
+                    }
+                   target = target.parentNode;
+                }
+                if (!show) {
+                    container.style.display = "none";
+                }
+            };
         }
         container.style.display = "block";
     };
-    //dateInput.onblur = function() {
-    //    container.style.display = "none";
-    //};
 }
+
